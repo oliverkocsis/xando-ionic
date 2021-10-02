b037906fb1f422dad5e5b96eae775eb30606e208
Oliver Kocsis <oliverkocsis@Olivers-MacBook-Pro.local>
2021-10-02 22:34:05 +0200

    Xando start page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,18 +5,9 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="ion-padding">
+        <h1>XandO</h1>
+        <p>Tic-tac-toe (American English), noughts and crosses (Commonwealth English), or Xs and Os (Irish English) is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. </p>
       </IonContent>
     </IonPage>
   );
