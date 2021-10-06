73cce38c0ef0272f47796e1d6032388f1d0f36dd
Oliver Kocsis <oliver.kocsis@gmail.com>
2021-10-06 22:47:05 +0200

    #4 Fix unit tests for Spaces and click on element

--- src/components/Space.test.tsx
+++ src/components/Space.test.tsx
@@ -3,31 +3,29 @@ import { render, fireEvent } from '@testing-library/react';
 import Space from './Space';
 import { _, X, O } from './Players';
 
-
-
 test('renders without crashing', () => {
-  const mark = jest.fn((index) => { });
+  const mark = jest.fn();
   const { baseElement } = render(<Space index={0} value={_} mark={mark}/>);
   expect(baseElement).toBeDefined();
 });
 
 test('empty space can be marked ', () => {
-  const mark = jest.fn((index) => { });
-  const { baseElement } = render(<Space index={0} value={_} mark={mark}/>);
-  fireEvent.click(baseElement);
+  const mark = jest.fn();
+  const { baseElement, getByText } = render(<Space index={0} value={_} mark={mark}/>);
+  fireEvent.click(getByText(_));
   expect(mark).toHaveBeenCalled();
 });
 
 test('marked space (x) can not be marked ', () => {
-  const mark = jest.fn((index) => { });
-  const { baseElement } = render(<Space index={0} value={X} mark={mark}/>);
-  fireEvent.click(baseElement);
+  const mark = jest.fn();
+  const { baseElement, getByText  } = render(<Space index={0} value={X} mark={mark}/>);
+  fireEvent.click(getByText(X));
   expect(mark).not.toHaveBeenCalled();
 });
 
 test('marked space (o) can not be marked ', () => {
-  const mark = jest.fn((index) => { });
-  const { baseElement } = render(<Space index={0} value={O} mark={mark}/>);
-  fireEvent.click(baseElement);
+  const mark = jest.fn();
+  const { baseElement, getByText  } = render(<Space index={0} value={O} mark={mark}/>);
+  fireEvent.click(getByText(O));
   expect(mark).not.toHaveBeenCalled();
 });
