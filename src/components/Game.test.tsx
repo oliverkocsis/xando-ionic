0444e3ead820e52d3fe13a19d387c5307926448a
Oliver Kocsis <oliver.kocsis@gmail.com>
2021-10-07 21:35:29 +0200

    #3 add unit tests for whoDidWin

--- src/components/Game.test.tsx
+++ src/components/Game.test.tsx
@@ -10,31 +10,81 @@ test('renders without crashing', () => {
 });
 
 test('empty: no winner', () => {
-  const winner = whoDidWin([]);
+  const spaces = [
+    _, _, _,
+    _, _, _,
+    _, _, _,
+  ]
+  const winner = whoDidWin(spaces);
   expect(winner).toBe(_);
 });
 
 test('x won horizontal', () => {
-  const winner = whoDidWin([]);
+  const spaces = [
+    X, X, X,
+    O, O, _,
+    _, _, _,
+  ]
+  const winner = whoDidWin(spaces);
   expect(winner).toBe(X);
 });
 
+test('o won horizontal', () => {
+  const spaces = [
+    X, X, _,
+    O, O, O,
+    X, _, _,
+  ]
+  const winner = whoDidWin(spaces);
+  expect(winner).toBe(X);
+});
+
+test('X won vertical', () => {
+  const spaces = [
+    O, X, _,
+    O, X, _,
+    _, X, _,
+  ]
+  const winner = whoDidWin(spaces);
+  expect(winner).toBe(O);
+});
+
 test('o won vertical', () => {
-  const winner = whoDidWin([]);
+  const spaces = [
+    O, X, X,
+    O, X, _,
+    O, _, _,
+  ]
+  const winner = whoDidWin(spaces);
   expect(winner).toBe(O);
 });
 
 test('x won diagonal', () => {
-  const winner = whoDidWin([]);
+  const spaces = [
+    X, O, _,
+    _, X, O,
+    _, _, X,
+  ]
+  const winner = whoDidWin(spaces);
   expect(winner).toBe(X);
 });
 
 test('o won diagonal', () => {
-  const winner = whoDidWin([]);
+  const spaces = [
+    X, X, O,
+    X, O, _,
+    O, _, _,
+  ]
+  const winner = whoDidWin(spaces);
   expect(winner).toBe(O);
 });
 
 test('full: no winner', () => {
-  const winner = whoDidWin([]);
+  const spaces = [
+    X, O, X,
+    O, X, O,
+    O, X, O,
+  ]
+  const winner = whoDidWin(spaces);
   expect(winner).toBe(_);
 });
